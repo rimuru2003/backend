8cbfec52fda75db47b2d9cf6990525258891afee
Aryan Singh <karansnh007@gmail.com>
2025-09-01 16:53:31 +0530

    .

--- day8/day8.js
+++
@@ -1,7 +0,0 @@
-const EventEmitter = require("events");
-
-const abc = new EventEmitter();
-
-abc.on("greet", (a) => console.log(`hello, name is  ${a.name} and iam a ${a.prof}`));
-
-abc.emit("greet", { name: "Aryan" , prof: "developer" });
