b246550031da28fbc8415b7e96f7139ebc29bd74
Aryan Singh <karansnh007@gmail.com>
2025-09-30 16:12:38 +0530

    mongo

--- express/small project/mongodb/mongodb-driver.js
+++ express/small project/mongodb/mongodb-driver.js
@@ -7,4 +7,40 @@ await Client.connect();
 const db = Client.db("mongo_db_nodejs");
 const userCollection = db.collection("user");
 
-userCollection.insertOne({ name: "Aryan ", surName: "Singh", age: 22 });
+// userCollection.insertOne({ name: "Aryan ", surName: "Singh", age: 22 });
+
+// userCollection.insertMany([
+//   { name: "Karan", surName: "Singh", age: 12, role: "user" },
+//   { name: "Akash", surName: "Singh", age: 32, role: "user" },
+//   { name: "patil", surName: "Singh", age: 52, role: "admin" },
+// ]);
+
+// read
+// const users = userCollection.find();
+
+// // console.log(users);
+
+// for await (const user of users) {
+//   console.log(user);
+// }
+
+// // read in single line
+// const user = await userCollection.find().toArray()
+// console.log(user)
+
+// use findone to fetch
+// const userdata = await userCollection.findOne({ surName: "Singh" });
+// console.log(userdata._id.toHexString());
+// console.log(userdata);
+
+// update
+
+//  await userCollection.updateOne(
+//   { name: "Karan" },
+//   { $set: { age: 30 } }
+// );
+
+
+// delete
+
+// await userCollection.deleteOne({name: "Karan "})
